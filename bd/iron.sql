f0a1a70b3aa3e42d9b529e1bc4917041ea33c53d
luis <lmontene@ucm.es>
2021-04-13 12:57:12 +0200

    bd

--- bd/iron.sql
+++ bd/iron.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-04-2021 a las 01:31:40
+-- Tiempo de generación: 13-04-2021 a las 12:54:19
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.2
 
@@ -25,10 +25,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(10) NOT NULL,
+  `comentario` varchar(500) NOT NULL,
+  `fecha` date NOT NULL,
+  `id_usuario` int(10) NOT NULL,
+  `id_hilo` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hilos`
+--
+
+CREATE TABLE `hilos` (
+  `id` int(10) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `fecha` date NOT NULL,
+  `mensaje` varchar(50) NOT NULL,
+  `id_U` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `id_tipo` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `roles`
 --
 
 CREATE TABLE `roles` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo`
+--
+
+CREATE TABLE `tipo` (
   `id` int(10) NOT NULL,
   `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -56,14 +107,54 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id`, `usuario`, `contraseña`, `nombre`, `apellidos`, `correo`, `rol_id`) VALUES
 (1, 'aaaa', 'aaaa', 'aaaa', 'aaaa', 'aaaa@ucm.es', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id` int(10) NOT NULL,
+  `id_producto` int(10) NOT NULL,
+  `id_usuario` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `comentario_fk_hilo` (`id_hilo`),
+  ADD KEY `comentario_fk_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `hilos`
+--
+ALTER TABLE `hilos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `hilo_fk_usuario` (`id_U`);
+
+--
+-- Indices de la tabla `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `productos_fk_tipo` (`id_tipo`);
+
+--
 -- Indices de la tabla `roles`
 --
 ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo`
+--
+ALTER TABLE `tipo`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -76,8 +167,34 @@ ALTER TABLE `usuarios`
   ADD KEY `rol_id` (`rol_id`);
 
 --
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `venta_fk_usuario` (`id_usuario`),
+  ADD KEY `venta_fk_producto` (`id_producto`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `hilos`
+--
+ALTER TABLE `hilos`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -86,20 +203,58 @@ ALTER TABLE `roles`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentario_fk_hilo` FOREIGN KEY (`id_hilo`) REFERENCES `hilos` (`id`),
+  ADD CONSTRAINT `comentario_fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `hilos`
+--
+ALTER TABLE `hilos`
+  ADD CONSTRAINT `hilo_fk_usuario` FOREIGN KEY (`id_U`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `productos_fk_tipo` FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id`);
 
 --
 -- Filtros para la tabla `roles`
 --
 ALTER TABLE `roles`
   ADD CONSTRAINT `roles_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`rol_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_fk_producto` FOREIGN KEY (`id_producto`) REFERENCES `produtos` (`id`),
+  ADD CONSTRAINT `venta_fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
