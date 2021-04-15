1a367ecf96868c95a1cdd245d3dac19b1de76ec3
hatercillo <79996838+hatercillo@users.noreply.github.com>
2021-04-15 23:09:30 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
     <h1 class="header-element">Baguettes of Iron</h1>
     <nav class="header-element">
         <a href="index.php">Inicio</a>
-        <a href="forum.php">Foros</a>
+        <a href="forum.php?pag=1">Foros</a>
         <a href="wiki.php">Wiki</a>
         <a href="store.php">Tienda</a>
         <a href="about.php">Sobre nosotros</a>
