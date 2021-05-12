5cbb3ad90b6e7a9b69b102c3eb426693186bbc76
unknown <antonio.garcia@appslopers.com>
2021-05-12 23:23:46 +0200

    fix estilos

--- about.php
+++ about.php
@@ -3,133 +3,177 @@
    $tituloPagina= 'Baguettes of Iron - Contacto';
 
    $contenido=<<<EOF
-   <section>
- <div>
-	<div>
-	<center><h1>Miembros</h1><center/>
-	</div>
-</div>
-
-<div>
-	<div>
-	<center><h3>¡Conoce al equipo que hay 
-	detras de "Baguettes of Iron"!<h3><center/>
-
-	<p><a href = "#Rafael">Rafael Marquez </a></p>
-	<p><a href = "#Ikram">Ikram Benali</a></p>
-	<p><a href = "#Luis">Luis Montenegro </a></p>
-	<p><a href = "#Mario">Mario Landabrou</a></p>
-	<p><a href = "#Pablo">Pablo Saiz </a></p>
-	<p><a href = "#Celso"> Antonio Garcia Pinedo</a></p>
-</div>
-	
-	
-<div>
-	<center><h2 id="Rafael">Rafael Marquez</h2><center/>
-	
-	<pre>
-	<center><img src = "img/rafael.jpg"><center/>
-	Nombre completo:Rafael Marquez Gonzalez
-	Correo Electronico:Rafmarqu@ucm.es
-	Descripcion:Soy un estudiante de ingeniería informática me gustan los videojuegos 
-	asi como los temas relacionados con la informatica y las peliculas 
-	de ciencia-ficcion	
-	
-	</pre>
-	</div>
-
-
-<div>
-	<center><h2 id = "Pablo">Pablo Saiz </h2><center/>
-	
-	<pre>
-	<center><img src="img/Pablo.jpg" width="100" height="100"><center/>
-	Nombre completo: Pablo Saiz Blázquez
-	Correo Electrónico: pabsaiz@ucm.es
-	Descripción : Mi afición es la música
-	
-	</pre>
-
-
-</div>
-
-
-
-
-<div>
-	<center><h2 id = "Mario">Mario Landaburu</h2><center/>
-	
-	<pre>
-	<center><img src = "img/Mario.jpg" width="100" height="100"><center/>
-	Nombre completo: Mario Landáburu Clares
-	correo electrónico: mariolan@ucm.es
-	Descripción : Aficionado a la historia y a los juegos de estrategia.
-	
-	</pre>
-</div>
-
-
-
-<div>
-	<center><h2 id = "Celso">Celso Antonio </h2><center/>
-	
-	<pre>
-	<center><img src = "img/Antonio.jpg" width="100" height="100"><center/>
-	Nombre completo:Celso Antonio Garcia Pinedo
-	correo electrónico: celsogar@ucm.es
-	Descripción : Estudiante de Ingeniería Informática ,aficionado a futbol , equipo favorito FC Barcelona.
-
-	</pre>
-</div>
-
-
-
-
-<div>	
-	<center><h2 id = "Luis"> Luis Montenegro</h2><center/>
-	
-		<pre>
-		<center><img src = "img/Luis.jpg" width="100" height="100"><center/>
-	Nombre completo: Luis Montenegro
-	correo electrónico: lmontene@ucm.es
-	Descripción :Le apasiona coleccionar sellos 
-
-		</pre>
-	
-</div>
-
-
-
-
-<div>
-	<center><h2 id = "Ikram">Ikram Benali</h2><center/>
-	
-	<pre>
-	<center><img src = "img/ikram.jpg" width="100" height="100"><center/>
-	Nombre completo:Ikram Benali
-	correo electrónico:ikramben@ucm.es
-	Descripción :Estudiando ingenieria informatica le gustan las peliculas romanticas 
-	ir al teatro y escuchar musica 
-
-	
-	</pre>
-</div>
-
-<style>
-div {
-  border: white 3px ;
-  margin: 10px;
-  padding: 10px;
-  background-color: lightgrey;
-}
-div div {
+   <h1>Miembros</h1>
+   <section class="miembros-about">
  
-  margin: 10px;
-  padding: 10px;
-  background-color: lightgrey;
-}
-</style>
+	
+	
+
+	<div class="row-miembros">
+		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				<img src="img/Antonio.jpg" alt="Avatar" style="width:300px;height:300px;">
+				</div>
+				<div class="flip-card-back">
+				<h1>Antonio Garcia</h1>
+				<p>Engineer and FullStack</p>
+				<p> celsogar@ucm.es</p>
+				<p>Estudiante de Ingeniería Informática ,aficionado a futbol , equipo favorito FC Barcelona.<p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				<img src="img/rafael.jpg" alt="Avatar" style="width:300px;height:300px;">
+				</div>
+				<div class="flip-card-back">
+				<h1>Rafael Marquez</h1>
+				<p>Engineer and FullStack</p>
+				<p> Rafmarqu@ucm.es</p>
+				<p>Soy un estudiante de ingeniería informática me gustan los videojuegos 
+				asi como los temas relacionados con la informatica y las peliculas 
+				de ciencia-ficcion<p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="flip-card">
+			<div class="flip-card-inner">
+				<div class="flip-card-front">
+				<img src="img/Pablo.jpg" alt="Avatar" style="width:300px;height:300px;">
+				</div>
+				<div class="flip-card-back">
+				<h1>Pablo Saiz</h1>
+				<p>Engineer and FullStack</p>
+				<p> pabsaiz@ucm.es</p>
+				<p>Mi afición es la música<p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row-miembros">
+	
+		<div class="flip-card">
+		<div class="flip-card-inner">
+			<div class="flip-card-front">
+			<img src="img/Mario.jpg" alt="Avatar" style="width:300px;height:300px;">
+			</div>
+			<div class="flip-card-back">
+			<h1>Mario Landaburu</h1>
+			<p>Engineer and FullStack</p>
+			<p> mariolan@ucm.es</p>
+			<p> Aficionado a la historia y a los juegos de estrategia.<p>
+			</div>
+		</div>
+		</div>
+		<div class="flip-card">
+		<div class="flip-card-inner">
+			<div class="flip-card-front">
+			<img src="img/Luis.jpg" alt="Avatar" style="width:300px;height:300px;">
+			</div>
+			<div class="flip-card-back">
+			<h1>Luis Montenegro</h1>
+			<p>Engineer and FullStack</p>
+			<p> lmontene@ucm.es</p>
+			<p> Le apasiona coleccionar sellos .<p>
+			</div>
+		</div>
+		</div>
+		
+		<div class="flip-card">
+		<div class="flip-card-inner">
+			<div class="flip-card-front">
+			<img src="img/ikram.jpg" alt="Avatar" style="width:300px;height:300px;">
+			</div>
+			<div class="flip-card-back">
+			<h1> Ikram Benali</h1>
+			<p>Engineer and FullStack</p>
+			<p> ikramben@ucm.es</p>
+			<p> Estudiando ingenieria informatica le gustan las peliculas romanticas 
+			ir al teatro y escuchar musica  .<p>
+			</div>
+		</div>
+		</div>
+	</div>
+
+
+
+
 
 </section>
+
+<style>
+
+h1{
+	text-align: center;
+}
+.miembros-about{
+	display:flex;
+	
+	flex-direction: column ;
+	justify-content: center ;
+}
+.row-miembros{
+	display:flex;
+	margin:4vh auto;
+	flex-direction: row ;
+	
+
+}
+.flip-card {
+	background-color: transparent;
+	width: 300px;
+	height: 300px;
+	perspective: 1000px;
+	
+	
+  }
+  
+  .flip-card-inner {
+	position: relative;
+	width: 100%;
+	height: 100%;
+	text-align: center;
+	transition: transform 0.6s;
+	transform-style: preserve-3d;
+	box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  }
+  
+  .flip-card:hover .flip-card-inner {
+	transform: rotateY(180deg);
+  }
+  
+  .flip-card-front, .flip-card-back {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	-webkit-backface-visibility: hidden;
+	backface-visibility: hidden;
+  }
+  
+  .flip-card-front {
+	background-color: #bbb;
+	color: black;
+  }
+  
+  .flip-card-back {
+	background-color: rgb(105, 76, 43);
+	color: white;
+	transform: rotateY(180deg);
+	
+  }
+  .flip-card-back h1{
+	  margin:10%;
+  }
+  .flip-card-back p{
+	  margin:10%;
+  }
+</style>
+
+
 EOF;
 require_once __DIR__.'/includes/plantillas/plantillaB.php'; 
