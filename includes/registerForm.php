dfcad216242e982ece451e2df9f9f8999981e105
PabloSaiz2 <55839661+PabloSaiz2@users.noreply.github.com>
2021-05-20 11:57:04 +0200

    Cambios de funcionalidad en el registro

--- includes/registerForm.php
+++ includes/registerForm.php
@@ -45,10 +45,15 @@
                 return array("Contraseña muy corta");
               else{
                 Usuario::crea($datos['user'],$datos['userpass'],$datos['name'],$datos['lastname'],$datos['email'],2);
-                $_SESSION['login'] = true;
-                $_SESSION['username'] = $datos['user'];
-                $_SESSION['rol']=2; 
-                return "index.php";
+                $created = Usuario::buscarUsuario($datos['user']);
+                if($created){
+                  $_SESSION['login'] = true;
+                  $_SESSION['username'] = $datos['user'];
+                  $_SESSION['rol']=2; 
+                  return "index.php";
+                }
+                else
+                  return array("No se ha podido crear el usuario");
               }
            }
            
