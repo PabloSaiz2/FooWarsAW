2ac80efd75c1c2b27901a8960379f1472c743aad
hatercillo <79996838+hatercillo@users.noreply.github.com>
2021-05-30 17:28:04 +0200

    Add files via upload

--- adminAddUser.php
+++ adminAddUser.php
@@ -1,35 +1,17 @@
 <?php
 include_once('includes/bootup.php');
 require_once('includes/validation.php');
+use es\fdi\ucm\aw\adminRegisterForm;
+$formulario = new AdminRegisterForm("registerForm");
+$form=$formulario->gestiona();
+$tituloPagina="Admin-Añadir usuario";
+$contenido=<<<EOF
+<div class="container">
+<h1>Añadir Usuario -Admin </h1>
+$form
+</div>
+<script type="text/javascript">
+</script>
+EOF;
+  require_once __DIR__.'/includes/plantillas/plantillaform.php'; 
 ?>
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="css/master.css?v=<?php echo (rand()); ?>">
-  <link rel="stylesheet" type="text/css" href="css/color.css?v=<?php echo (rand()); ?>">
-  <script src="https://kit.fontawesome.com/0afaf0fad2.js" crossorigin="anonymous"></script>
-  <title>Admin-Añadir usuario</title>
-</head>
-
-<body>
-  <?php
-  include_once('includes/comun/header.php');
-  ?>
-  <div class="container">
-    <h1>Añadir Usuario -Admin </h1>
-    <?php
-
-    use es\fdi\ucm\aw\AdminRegisterForm;
-
-    $formulario = new AdminRegisterForm("registerForm");
-    $formulario->gestiona();
-    ?>
-  </div>
-  <script type="text/javascript">
-  
-  </script>
-</body>
-
-</html>
--- insert.php
+++ insert.php
@@ -1,27 +1,17 @@
 <?php
-    include_once('includes/bootup.php');
-    require_once('includes/validation.php');
+include_once('includes/bootup.php');
+require_once('includes/validation.php');
+use  es\fdi\ucm\aw\insertForm;
+$formulario = new InsertForm("insertForm");
+$form=$formulario->gestiona();
+$tituloPagina="añadir producto";
+$contenido=<<<EOF
+<div class="container">
+<h1>Añadir Producto</h1>
+$form
+</div>
+<script type="text/javascript">
+</script>
+EOF;
+require_once __DIR__.'/includes/plantillas/plantillaform.php';
 ?>
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type = "text/css" href="css/master.css?v=<?php echo(rand());?>">
-    <link rel="stylesheet" type = "text/css" href="css/color.css?v=<?php echo(rand());?>">
-    <script src="https://kit.fontawesome.com/0afaf0fad2.js" crossorigin="anonymous"></script>
-    <title>Baguettes of Iron - Login</title>
-  </head>
-  <body>
-      <?php
-        include_once('includes/comun/header.php');
-      ?>
-      <div class="container">
-          <h1>Añadir Producto</h1>
-                <?php
-                  use  es\fdi\ucm\aw\InsertForm;
-                  $formulario = new InsertForm("insertForm");
-                  $formulario->gestiona();
-                ?>
-      </div>
-  </body>
-</html>
